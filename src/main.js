f81c3775d48f9e2d1fa203d8253e88e99a79d8d0
Erik Perri <erik.perri@gmail.com>
2020-05-15 19:15:46 -0400

    Added data attribute based initialization

--- src/main.js
+++ src/main.js
@@ -1,2 +1,41 @@
-// eslint-disable-next-line
+import Monitor from './Monitor';
+
+const { document } = window;
+const automaticOverflowMenus = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('[data-overflow-menu-items]').forEach((element) => {
+    const menuItemSelector = element.dataset.overflowMenuItems;
+    if (!menuItemSelector) {
+      console.warn('Missing selector', element);
+      return;
+    }
+
+    const menuItemElement = element.querySelector('[data-overflow-menu-more-item]');
+    const menuContainerElement = element.querySelector('[data-overflow-menu-more-container]');
+    if (!menuItemElement || !menuContainerElement) {
+      console.warn('Missing overflow child', element);
+      return;
+    }
+
+    // noinspection JSCheckFunctionSignatures
+    const monitor = new Monitor(element, menuItemSelector, menuItemElement, menuContainerElement);
+
+    monitor.start();
+
+    automaticOverflowMenus.push({ element, monitor });
+  });
+});
+
+// noinspection JSUnusedGlobalSymbols
+/**
+ * @param {HTMLElement} element
+ */
+export const FindMonitor = (element) => {
+  const info = automaticOverflowMenus.find((i) => i.element === element);
+
+  return info ? info.monitor : null;
+};
+
+// noinspection JSUnusedGlobalSymbols
 export { default as Monitor } from './Monitor';
