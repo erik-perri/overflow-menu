2964e0664645288f23f4b971f80a63bd701b235f
Erik Perri <erik.perri@gmail.com>
2020-05-15 23:37:30 -0400

    Removed non-browser builds

--- rollup.config.js
+++ rollup.config.js
@@ -28,17 +28,4 @@ export default [
         : [],
     ],
   },
-  {
-    input: 'src/main.js',
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-    ],
-    plugins: [
-      babel({
-        babelHelpers: 'bundled',
-        exclude: ['node_modules/**'],
-      }),
-    ],
-  },
 ];
