70bb363e18a89d9ff9c1f305070f899a9e392eac
Erik Perri <erik.perri@gmail.com>
2020-05-29 21:01:20 -0400

    Rearranged setup

--- src/index.ts
+++ src/index.ts
@@ -7,43 +7,6 @@ const overflowContainerSelector = '[data-overflow-menu-more-container]';
 const menus: { menu: OverflowMenuInterface; element: HTMLElement }[] = [];
 const monitor: ResizeMonitor = new ResizeMonitor(window);
 
-const createMenus = (): void => {
-  window.document.querySelectorAll(itemContainerSelector)
-    .forEach((element: Element) => {
-      const htmlElement = element as HTMLElement;
-
-      const menuItemSelector: string = htmlElement.dataset.overflowMenuItems || '';
-      if (!menuItemSelector) {
-        if (process.env.NODE_ENV === 'development') {
-          console.warn('Missing selector', htmlElement);
-        }
-        return;
-      }
-
-      const overflowItem = htmlElement.querySelector(overflowItemSelector);
-      const overflowContainer = htmlElement.querySelector(overflowContainerSelector);
-      if (!overflowItem || !overflowContainer) {
-        if (process.env.NODE_ENV === 'development') {
-          console.warn('Missing overflow child', element);
-        }
-        return;
-      }
-
-      const menu = new OverflowMenu({
-        itemContainer: htmlElement,
-        itemSelector: menuItemSelector,
-        overflowItem: overflowItem as HTMLElement,
-        overflowContainer: overflowContainer as HTMLElement,
-      });
-
-      menus.push({ menu, element: htmlElement });
-    });
-
-  if (menus.length) {
-    monitor.start();
-  }
-};
-
 monitor.onRecalculateSizes((): void => {
   menus.forEach((info) => {
     info.menu.refreshSizes();
@@ -54,6 +17,42 @@ monitor.onRecalculateSizes((): void => {
 monitor.onResize((): void => {
   menus.forEach((info) => info.menu.refreshItems());
 });
+
+const createMenus = (): void => {
+  window.document.querySelectorAll(itemContainerSelector).forEach((element: Element) => {
+    const htmlElement = element as HTMLElement;
+
+    const menuItemSelector: string = htmlElement.dataset.overflowMenuItems || '';
+    if (!menuItemSelector) {
+      if (process.env.NODE_ENV === 'development') {
+        console.warn('Missing selector', htmlElement);
+      }
+      return;
+    }
+
+    const overflowItem = htmlElement.querySelector(overflowItemSelector);
+    const overflowContainer = htmlElement.querySelector(overflowContainerSelector);
+    if (!overflowItem || !overflowContainer) {
+      if (process.env.NODE_ENV === 'development') {
+        console.warn('Missing overflow child', element);
+      }
+      return;
+    }
+
+    const menu = new OverflowMenu({
+      itemContainer: htmlElement,
+      itemSelector: menuItemSelector,
+      overflowItem: overflowItem as HTMLElement,
+      overflowContainer: overflowContainer as HTMLElement,
+    });
+
+    menus.push({ menu, element: htmlElement });
+  });
+
+  if (menus.length) {
+    monitor.start();
+  }
+};
 
 const { readyState } = window.document;
 
